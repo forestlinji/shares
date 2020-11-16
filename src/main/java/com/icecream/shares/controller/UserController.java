ab4a35f659d27d1b795c627f68a348d936a7c09f
forestlinji <2770987304@qq.com>
2020-11-16 19:32:48 +0800

    add UserController

---
+++ src/main/java/com/icecream/shares/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.icecream.shares.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
--- src/test/java/com/icecream/shares/SharesApplicationTests.java
+++ src/test/java/com/icecream/shares/SharesApplicationTests.java
@@ -1,5 +1,7 @@
 package com.icecream.shares;
 
+import com.baomidou.mybatisplus.core.toolkit.EncryptUtils;
+import com.icecream.shares.utils.MD5Utils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class SharesApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(MD5Utils.cptry("1" + "123456" + "1"));
     }
 
 }
