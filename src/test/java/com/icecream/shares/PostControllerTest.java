8d22790ff06a4cd81ebfacb4d715435aed678101
forestlinji <2770987304@qq.com>
2020-11-24 15:38:34 +0800

    add test

---
+++ src/test/java/com/icecream/shares/PostControllerTest.java
@@ -0,0 +1,13 @@
+package com.icecream.shares;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class PostControllerTest {
+    public final String baseUrl = "http://localhost:11451";
+    public String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJST0xFIjoidXNlciIsImlzcyI6IkZvcmVzdGpDbGltYiIsImlhdCI6MTYwNjExODQyOSwic3ViIjoiMyIsImV4cCI6MTYwODcxMDQyOX0.-dadapsfD1hWZ8EtjxKtA0p3yEx90vf7cXC_aL2RlW4";
+}
--- src/test/java/com/icecream/shares/SharesApplicationTests.java
+++ src/test/java/com/icecream/shares/SharesApplicationTests.java
@@ -3,9 +3,13 @@ package com.icecream.shares;
 import com.baomidou.mybatisplus.core.toolkit.EncryptUtils;
 import com.icecream.shares.utils.MD5Utils;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class SharesApplicationTests {
 
     @Test
